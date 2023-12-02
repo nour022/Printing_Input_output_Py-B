7d53801ea9ca25fb3942f4a482c2334ea2d05b3b
Nour <gmn09000@gmail.com>
2023-12-02 13:48:02 +0100

    done+

---
+++ main.py
@@ -0,0 +1,18 @@
+#Ex1 printing Exercise
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+#Ex2 printing Exercise
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+#Ex3  input Exercise
+# input() : is a fanuction for inpute
+# len() : is a function how count the length one array 'Anzahl der char
+print(len(input("What is your Name? ")))
+
+# Varibel 
+name="Nour"  #  Sting 
+num=200      #  Integer 
+num_f=1.2    #  Float
